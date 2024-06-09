f6d566f75945b9aed0b7c6eebc057a581579baf0
yomi8802 <morita8002@gmail.com>
2024-06-09 22:26:14 +0900

    base変更

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/colorMaker/',
+  base: 'https://yomi8802.github.io/colorMaker/',
 });
